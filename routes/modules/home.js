00fd3c4d1245cb09386deada80289f8bb3f439a2
LeeChan <lee79102@hotmail.com>
2020-12-21 02:37:50 +0800

    feat: generateLink checkDupe function done

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -13,37 +13,27 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const { originLink } = req.body
   let shortenLink = generateLink()
-  console.log('shortenLink:', shortenLink)
   Link.findOne()
     .or([{ originLink }, { shortenLink }])
     .lean()
     .then(link => {
-      console.log('Boolean:', Boolean(link))
-      console.log('originLink:', originLink)
-      console.log('shortenLink:', shortenLink)
       console.log('1st:', link)
       if (!link) { // 如果已經有轉換過，就調用資料庫記錄
         Link.create({ originLink, shortenLink })
           .then(link => {
-            console.log('Create Link:', link)
             res.render('index', { originLink, shortenLink })
           })
           .catch(error => console.log(error))
       } else if (link.originLink === originLink) { // 如果還沒轉換過，就啟動generateLink函式來新增一筆資料
-        console.log('Find originLink:', link)
-        console.log('Find originLink:', link.shortenLink)
         res.render('index', { originLink: link.originLink, shortenLink: link.shortenLink })
       } else if (link.shortenLink === shortenLink) {
         let checkDupe
         do {
           shortenLink = generateLink()
           checkDupe = link.shortenLink === shortenLink
-          console.log(link.shortenLink)
-          console.log(shortenLink)
         } while (checkDupe)
         Link.create({ originLink, shortenLink })
           .then(link => {
-            console.log('Create Link:', link)
             res.render('index', { originLink, shortenLink })
           })
           .catch(error => console.log(error))
@@ -51,36 +41,6 @@ router.post('/', (req, res) => {
     })
     .catch(error => console.log('Error', error))
 })
-/*
-// 定義主功能路由
-router.post('/', (req, res) => {
-  const originLink = req.body.originLink // 先抓住表單送出的網址
-  Link.findOne({ originLink }) // 再以該網址於資料庫內搜尋
-    .lean() // 調整資料格式
-    .then(link => {
-      console.log('1st:', link)
-      if (link) { // 如果已經有轉換過，就調用資料庫記錄
-        res.render('index', { originLink, shortenLink: link.shortenLink })
-      } else { // 如果還沒轉換過，就啟動generateLink函式來新增一筆資料
-        let shortenLink = generateLink
-        Link.findOne({ shortenLink }) // 再以該網址於資料庫內搜尋
-          .lean() // 調整資料格式
-          .then(link => {
-            console.log('2st:', link)
-            if (link) { // 如果曾經使用過，就再次調動generateLink函式來新增一筆資料
-              shortenLink = generateLink
-            } else { // 如果未曾使用過，就以此為新資料
-              Link.create({ originLink, shortenLink })
-                .then(link => {
-                  res.render('index', { originLink: link.originLink, shortenLink: link.shortenLink })
-                })
-            }
-          })
-      }
-    })
-    .catch(error => console.log(error))
-})
-*/
 
 router.get('/:shortenLink', (req, res) => {
   const shortenLink = `${ROOTDOMAIN}/${req.params.shortenLink}`
